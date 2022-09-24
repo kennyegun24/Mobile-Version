0e122d0eeb4ece96aa1450aee8e21e1eeabdad84
kenny Egun <kennyegun240@gmail.com>
2022-09-24 10:42:16 +0100

    Added a click event to see resume to display CV

--- main.js
+++ main.js
@@ -290,3 +290,147 @@ Email.addEventListener('input', (e) => {
 Texted.addEventListener('input', (e) => {
   localStorage.setItem('formText', e.target.value);
 });
+
+// CV DISPLAY
+const certs = [{
+  certImage: 'expe6gzj_1663585247656[1].svg',
+  certDescription: 'HTML and CSS certificate of completion. Issued by Microvese',
+}, {
+  certImage: 'zz1r9nox_1660230534588[1](3).svg',
+  certDescription: 'Intro to Web Development. Issued by Microvese',
+},
+];
+
+const btn3 = document.querySelector('.btn3');
+const curriculaVitae = document.querySelector('.curriculaVitae');
+const foot = document.querySelector('.foot');
+const section2 = document.querySelector('.section2');
+const abouts = document.querySelector('.about');
+const section1 = document.querySelector('.section1');
+
+btn3.addEventListener('click', () => {
+  curriculaVitae.style.display = 'block';
+  curriculaVitae.innerHTML = `
+  <div class="cvDiv"> 
+  <div class="cvDivTwo">
+      <img src="/images/expe6gzj_1663585247656[1].svg" class="cvPassport" width="100%" alt="">
+      <h3 class="cvDivTwoHeader">
+          Contact
+      </h3>
+      <hr class="curLine">
+      <ul class="cvDivTwoUl">
+          <li>Blk N18, Flat 3, Abesan Estate</li>
+          <li>Ipaja,</li>
+          <li>Lagos State</li>
+          <li>Nigeria.</li>
+      </ul>
+      <ul class="cvDivTwoUl2">
+          <li>+234-80-254-6478-9</li>
+          <li>kennyegun240@email.com</li>
+          <li><a href="">kenny_eLiasUTD</a></li>
+      </ul>
+
+      <h3 class="cvDivTwoHeader2">
+          Skills
+      </h3>
+      <hr class="curLine">
+      <ul class="cvDivTwoUl3">
+          <li>Communication Skills</li>
+          <li>Leadership Skills</li>
+          <li>Ability to work alone</li>
+          <li>Team Leadership</li>
+          <li>Goals Oriented</li>
+          <li>Team Building</li>
+          <li>Positive Value</li>
+      </ul>
+  </div>
+  <div class="cvDivThree">
+      <div class="cvHam">
+          <div class="cvHam1"></div>
+          <div class="cvHam2"></div>
+          <div class="cvHam3"></div>
+        </div>
+      <h2 class="cvDivThreeHeader">
+          Kenny Egun
+      </h2>
+      <h3 class="cvDivThreeHeader2">
+          Software Developer
+      </h3>
+      <ul class="cvDivThreeUl">
+          <li class="cvVert">Male</li>
+          <li class="cvVert">Jan 29 2001</li>
+          <li>Lagos, Nigeria</li>
+      </ul>
+      <h3 class="cvDivThreeHeader3">
+          Citizenship: Nigeria
+      </h3>
+      <p class="cvPara">
+          I decided to venture into the software development world, 
+          full stack to be precise, early this year. I started learning python before 
+          I decided to start from scratch with HTML and CSS. It was a very great journey learning new skills, 
+          and I am still learning new skills to top up myself. Furthermore, 
+          I am able to work in a remote environment as this was part of my training 
+          in the institution I learned how to code.
+      </p>
+      <h3 class="cvDivThreeHeader4">
+          Education History
+      </h3>
+      <hr class="curLine2">
+      <ul class="cvDivThreeUl2">
+          <li class="natDip">Higher National Diploma</li>
+          <li class="yabaTech">Yaba College of Technology</li>
+          <li class="yabaTech">Year 2020 - present</li>
+      </ul>
+      <h3 class="cvDivThreeHeader5">
+          Languages and Frameworks
+      </h3>
+      <hr class="curLine2">
+      <ul class="cvDivThreeUl3">
+          <li>HTML</li>
+          <li>CSS</li>
+          <li>JavaScript</li>
+          <li>Ruby</li>
+          <li>React</li>
+          <li>Redux</li>
+          <li>BootStrap</li>
+          <li>Rails</li>
+      </ul>
+      <h3 class="cvDivThreeHeader5">
+          Certifications
+      </h3>
+      <hr class="curLine2">
+      <div class="cvImagesDiv">
+      </div>
+  </div>
+</div>`;
+  const cvImagesDiv = document.querySelector('.cvImagesDiv');
+  certs.forEach((cert) => {
+    const ul = document.createElement('ul');
+    ul.innerHTML = `
+              <li class="cvDivCerts">${cert.certDescription}</li>
+          <img src="/images/${cert.certImage}" class="cvImages" alt="">
+          `;
+    cvImagesDiv.classList.add('cvImagesDiv');
+    cvImagesDiv.append(ul);
+
+    foot.style.display = 'none';
+    section2.style.display = 'none';
+    abouts.style.display = 'none';
+    section1.style.display = 'none';
+
+    const cvHam = document.querySelector('.cvHam');
+
+    cvHam.addEventListener('click', () => {
+      curriculaVitae.style.display = 'none';
+      foot.style.display = 'block';
+      section2.style.display = 'block';
+      abouts.style.display = 'block';
+      const { width } = window.screen;
+      if (width > 765) {
+        section1.style.display = 'flex';
+      } else {
+        section1.style.display = 'block';
+      }
+    });
+  });
+});
